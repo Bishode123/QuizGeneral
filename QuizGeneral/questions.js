5ab5e5638b624c0c118649721dd11fa8bde66c56
bishodevrrudra <bishodevrrudra@outlook.com>
2018-01-09 08:48:12 -0800

    Add project files.

---
+++ QuizGeneral/questions.js
@@ -0,0 +1,51 @@
+var Questions = { "question" : [
+  {
+    "question"  : " How many Bytes are stored by ‘Long’ Datatype in C# .net?",
+     "choice1"  : "8",
+     "choice2"  : "4",
+     "choice3"  : "2",
+     "choice4"  : "1",
+     "choice5"  : "16",                    
+     "correct"  : 1
+  },
+  
+  {
+    "question"  : "To generate a simple notification for an object in runtime, the programming construct to be used for implementing this idea?",
+     "choice1"  : "namespace",
+     "choice2"  : "interface",
+     "choice3"  : "attribute",
+     "choice4"  : "delegate",
+     "choice5"  : "anonymous type",                    
+     "correct"  : 4
+  },
+  
+  {
+    "question"  : "Which of these methods of the class String is used to obtain length of String object?",
+     "choice1"  : "get()",
+     "choice2"  : "sizeof()",
+     "choice3"  : "lengthof()",
+     "choice4"  : "length",
+     "choice5"  : "load()",                    
+     "correct"  : 4
+  },
+  
+  {
+    "question"  : "When no exception is thrown at runtime then who will catch it?",
+     "choice1"  : "CLR",
+     "choice2"  : "Operating System",
+     "choice3"  : "Loader",
+     "choice4"  : "Compiler",
+     "choice5"  : "None of These",                    
+     "correct"  : 1
+  },
+  
+  {
+    "question"  : " Which of the following is not a namespace in the .NET Framework Class Library?",
+     "choice1"  : "System.Process",
+     "choice2"  : "System.Security",
+     "choice3"  : "System.Threadin",
+     "choice4"  : "System.xml",
+     "choice5"  : "System.Data",                    
+     "correct"  : 1
+  }
+]};
